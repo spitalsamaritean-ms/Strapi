19b181074af75e0a79515481d38670429dca4061
Atesz23 <163830561+spitalsamaritean-ms@users.noreply.github.com>
2024-03-20 17:27:22 +0200

    remove blog image

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -842,9 +842,6 @@ export interface ApiBlogBlog extends Schema.CollectionType {
       ]
     >;
     seo: Attribute.Component<'shared.seo'>;
-    image: Attribute.Media;
-    top: Attribute.Boolean & Attribute.DefaultTo<false>;
-    priority: Attribute.Integer;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
