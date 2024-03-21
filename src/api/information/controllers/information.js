97705b652352cc249cd57dd07eb890121cc64c21
Atesz23 <163830561+spitalsamaritean-ms@users.noreply.github.com>
2024-03-21 13:41:40 +0200

    informations by slug

--- src/api/information/controllers/information.js
+++ src/api/information/controllers/information.js
@@ -1,9 +1,26 @@
-'use strict';
+"use strict";
 
 /**
  * information controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::information.information');
+module.exports = createCoreController(
+  "api::information.information",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+
+      const entity = await strapi.db
+        .query("api::information.information")
+        .findOne({
+          where: { slug: id },
+          populate: ["deep"],
+        });
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
