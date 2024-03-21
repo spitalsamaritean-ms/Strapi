0d61d555b5b8e683ba76684a519d79b5749d1d1a
Atesz23 <163830561+spitalsamaritean-ms@users.noreply.github.com>
2024-03-21 12:38:03 +0200

    blog slug

--- src/api/blog/controllers/blog.js
+++ src/api/blog/controllers/blog.js
@@ -1,9 +1,21 @@
-'use strict';
+"use strict";
 
 /**
  * blog controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::blog.blog');
+module.exports = createCoreController("api::blog.blog", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.db.query("api::blog.blog").findOne({
+      where: { slug: id },
+      populate: ["deep"],
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
