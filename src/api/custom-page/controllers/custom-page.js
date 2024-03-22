5d09aba27013405ea4a2f75920eb8d52840333a6
Atesz23 <163830561+spitalsamaritean-ms@users.noreply.github.com>
2024-03-22 14:38:19 +0200

    customPage find by slug

--- src/api/custom-page/controllers/custom-page.js
+++ src/api/custom-page/controllers/custom-page.js
@@ -1,9 +1,26 @@
-'use strict';
+"use strict";
 
 /**
  * custom-page controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::custom-page.custom-page');
+module.exports = createCoreController(
+  "api::custom-page.custom-page",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+
+      const entity = await strapi.db
+        .query("api::custom-page.custom-page")
+        .findOne({
+          where: { slug: id },
+          populate: ["deep"],
+        });
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
