65bc51fb2b5fe920c93ffd4d5da7787203e63111
Atesz23 <163830561+spitalsamaritean-ms@users.noreply.github.com>
2024-04-03 16:53:45 +0300

    info patient slug

--- src/api/info-patient/controllers/info-patient.js
+++ src/api/info-patient/controllers/info-patient.js
@@ -1,9 +1,26 @@
-'use strict';
+"use strict";
 
 /**
  * info-patient controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::info-patient.info-patient');
+module.exports = createCoreController(
+  "api::info-patient.info-patient",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+
+      const entity = await strapi.db
+        .query("api::info-patient.info-patient")
+        .findOne({
+          where: { slug: id },
+          populate: ["deep"],
+        });
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
